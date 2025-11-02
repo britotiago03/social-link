332854d26b7a898d6fe7b4f026637b97e6e66e01
Tiago Brito <69848652+britotiago03@users.noreply.github.com>
2025-11-02 13:33:55 +0100

    Created main page in web-server

---
+++ web-server/src/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Roboto, Imprima } from "next/font/google";
+
+export const roboto = Roboto({ subsets:['latin'] });
+export const imprima = Imprima({
+    weight: ['400'],
+    subsets: ['latin']
+});
